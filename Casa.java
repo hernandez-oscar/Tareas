dc0d7c0b46e65b53aea55a06fa60239b4e478f2d
hernandez-oscar <60532065+hernandez-oscar@users.noreply.github.com>
2020-02-26 22:26:24 -0600

    Add files via upload

---
+++ Casa.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package casa;
+import casa;
+/**
+ *
+ * @author infin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        casa casa1=new casa();
+        System.out.println(casa1.toString());
+                
+    }
+    
+}
